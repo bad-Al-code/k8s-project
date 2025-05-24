073eddb52abc135876d9c9aa7eb17f4e8cf38144
Badal <badalsingh120403@gmail.com>
2025-05-24 01:25:25 -0500

    feat: create promotheous metrics configuration

--- services/user-api/src/app.ts
+++ services/user-api/src/app.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import winston from "winston";
+import client from "prom-client";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -15,8 +16,38 @@ const logger = winston.createLogger({
   ],
 });
 
+const collectDefaultMetrics = client.collectDefaultMetrics;
+const Registry = client.Registry;
+const register = new Registry();
+
+collectDefaultMetrics({ register });
+
+const totalRequests = new client.Counter({
+  name: "user_api_total_requests",
+  help: "Total number of request to the user-api services",
+  labelNames: ["method", "route", "status_code"],
+  registers: [register],
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.on("finish", () => {
+    totalRequests.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+  });
+
+  next();
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello");
+});
+
+app.get("/metrics", async (req: Request, res: Response) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.end(await register.metrics());
 });
 
 app.listen(PORT, () => {
