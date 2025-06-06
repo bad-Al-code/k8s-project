7f048662c7b7ef579d0f9b5ad7af3efbf71b03a5
Badal <badalsingh120403@gmail.com>
2025-06-06 17:28:40 -0500

    test: add jest configuration

---
+++ simple-microservice/video-streaming-svc/src/math.js
@@ -0,0 +1,3 @@
+export const square = (n) => {
+  return n * n;
+};
---
+++ simple-microservice/video-streaming-svc/src/math.test.js
@@ -0,0 +1,8 @@
+import { square } from "./math";
+
+describe("square function", () => {
+  test("can square two", () => {
+    const result = square(2);
+    expect(result).toBe(4);
+  });
+});
